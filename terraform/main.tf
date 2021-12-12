7fbdcc6b2ed944c668761b24bb5a779f7cb32444
Espen Hermansen <espen.hermansen@gmail.com>
2021-12-12 22:08:21 +0100

    Add tf files

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,11 @@
+resource "digitalocean_kubernetes_cluster" "k8s" {
+  name    = "do-kubernetes"
+  region  = "ams3"
+  version = "1.21.5-do.0"
 
+  node_pool {
+    name       = "do-kube-nodes"
+    size       = "s-1vcpu-2gb"
+    node_count = "2"
+  }
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,5 @@
-
+variable "do_token" {
+  description = "Digital Ocean Personal access token"
+  type        = string
+  default     = ""
+}
